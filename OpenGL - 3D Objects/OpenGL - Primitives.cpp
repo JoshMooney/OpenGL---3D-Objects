6e4182127304ef9619fdf0a6409a1bbf64812ef0
Joshmoo2009 <joshmoo2009@hotmail.com>
2015-01-12 10:41:05 +0000

    First Commit

---
+++ OpenGL - 3D Objects/OpenGL - Primitives.cpp
@@ -0,0 +1,47 @@
+// OpenGL - Primitives.cpp : Defines the entry point for the console application.
+//
+
+//////////////////////////////////////////////////////////// 
+// Headers 
+//////////////////////////////////////////////////////////// 
+#include "stdafx.h" 
+#ifdef _DEBUG 
+#pragma comment(lib,"sfml-graphics-d.lib") 
+#pragma comment(lib,"sfml-audio-d.lib") 
+#pragma comment(lib,"sfml-system-d.lib") 
+#pragma comment(lib,"sfml-window-d.lib") 
+#pragma comment(lib,"sfml-network-d.lib") 
+#else 
+#pragma comment(lib,"sfml-graphics.lib") 
+#pragma comment(lib,"sfml-audio.lib") 
+#pragma comment(lib,"sfml-system.lib") 
+#pragma comment(lib,"sfml-window.lib") 
+#pragma comment(lib,"sfml-network.lib") 
+#endif 
+#pragma comment(lib,"opengl32.lib") 
+#pragma comment(lib,"glu32.lib") 
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	sf::RenderWindow window(sf::VideoMode(800, 600, 32), "OpenGL - 3D Objects");
+
+	while (window.isOpen())
+	{
+		// Process events 
+		sf::Event Event;
+		while (window.pollEvent(Event))
+		{
+			// Close window : exit 
+			if (Event.type == sf::Event::Closed)
+				window.close();
+		}
+		//prepare frame
+		window.clear();
+		window.setFramerateLimit(500);
+
+		window.display();
+	}
+	return EXIT_SUCCESS;
+}
+
